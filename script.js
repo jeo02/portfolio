98c9cb848523361ed1c65c279e4899b28e984dd8
jeo02 <juan.ospina@stonybrook.edu>
2021-05-19 20:13:15 -0400

    About Me section
    Big changes to the about me section, definitely needs some touch ups but it is fair so far.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-document.getElementById("container").style.gridTemplateRows = "175px 175px 200px" + document.getElementById("image1").clientHeight + "200px";
+function gridResize(){
+    document.getElementById("container").style.gridTemplateRows = "175px 175px 200px " + (document.getElementById("image1").clientHeight * 0.5) + "px 1000px " + 
+    (document.getElementById("image1").clientHeight * 0.5) + "px";
+}
+
+document.addEventListener('scroll', function(){
+    let y = window.pageYOffset;
+    var element_position = document.getElementById("aboutMe").offsetTop;
+    if(y > element_position){
+        document.getElementById("image1").src = "track_pic.JPG";
+    }
+    else
+        document.getElementById("image1").src = "stony_pic.jpg";
+})
+var slideIndex = 1;
+
+window.addEventListener('load', function(){
+    showDivs(slideIndex);
+})
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
